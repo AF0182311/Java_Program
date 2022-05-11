e89687ed6ac1630a36c70f707b42775029284686
AF0182311 <105125534+AF0182311@users.noreply.github.com>
2022-05-11 21:25:32 +0530

    Add files via upload

---
+++ Nested3.java
@@ -0,0 +1,19 @@
+class Nested3
+{
+public static void main(String args[])
+{
+int i=1;
+    while(i<=5)
+   {
+    int j=5;
+        while(j>=i)
+           {
+                System.out.print("*"+" ");
+                j--;
+            }
+           System.out.println();
+          i++;
+      }
+    
+}
+}
---
+++ Number.java
@@ -0,0 +1,17 @@
+class Number
+{
+public static void main(String args[])
+{
+int i=1;
+int count=876;
+  while(i<=854)
+      {
+     System.out.println(count);
+    count--;
+      i++;
+      }
+
+
+
+}
+}
