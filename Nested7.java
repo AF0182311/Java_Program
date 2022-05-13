fbe320868d8e9314f83fb1cf25ee888f951a2bf7
AF0182311 <105125534+AF0182311@users.noreply.github.com>
2022-05-13 21:41:39 +0530

    Add files via upload

---
+++ Nested7.java
@@ -0,0 +1,43 @@
+/*  *
+   * *
+  * * *
+ * * * *
+* * * * * 
+   
+*/
+
+class Nested7
+{
+public static void main(String args[])
+{
+for(int i=1;i<=5;i++)
+{
+   for(int j=i;j<5;j++)
+     {
+        System.out.print(" ");
+     }
+    for(int k=1;k<2*i;k++)
+       {
+         if(k%2==0)
+           {
+             System.out.print(" ");
+            }
+         else
+           {
+            System.out.print("*");
+            }
+       }
+
+System.out.println();
+
+}
+}
+}
+
+
+
+
+
+
+
+
