2eeed701fb5da55ddf5961f04d77f252fb40eb94
Rajneesh <105125534+AF0182311@users.noreply.github.com>
2022-07-06 15:03:51 +0530

    Add files via upload

---
+++ java/Vowel.java
@@ -0,0 +1,43 @@
+package three;
+
+public class Vowel {
+	public void vowel(char ch)
+	{
+	 switch(ch)  
+	    {  
+	        case 'a':   
+	            System.out.println("Vowel");  
+	            break;  
+	        case 'e':   
+	            System.out.println("Vowel");  
+	            break;  
+	        case 'i':   
+	            System.out.println("Vowel");  
+	            break;  
+	        case 'o':   
+	            System.out.println("Vowel");  
+	            break;  
+	        case 'u':   
+	            System.out.println("Vowel");  
+	            break;  
+	        case 'A':   
+	            System.out.println("Vowel");  
+	            break;  
+	        case 'E':   
+	            System.out.println("Vowel");  
+	            break;  
+	        case 'I':   
+	            System.out.println("Vowel");  
+	            break;  
+	        case 'O':   
+	            System.out.println("Vowel");  
+	            break;  
+	        case 'U':   
+	            System.out.println("Vowel");  
+	            break;  
+	        default:   
+	            System.out.println("Consonant");  
+	    }  
+	}    
+	   
+}
